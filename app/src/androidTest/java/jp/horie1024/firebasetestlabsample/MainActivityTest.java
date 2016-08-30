b99b12f848c842b938e9fce84af0c7dd8da51f4e
horie1024 <ryosuke.pic@gmail.com>
2016-08-30 18:04:28 +0900

    first commit

---
+++ app/src/androidTest/java/jp/horie1024/firebasetestlabsample/MainActivityTest.java
@@ -0,0 +1,31 @@
+package jp.horie1024.firebasetestlabsample;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * @author horie
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void isShowHelloWorldText() {
+        onView(withId(R.id.login)).perform(click());
+        onView(withText(R.string.hello_world)).check(matches(isDisplayed()));
+    }
+}
